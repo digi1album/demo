b3bdcd6758220c5799134cd2900f8d643ce19c12
digi1album <143440886+digi1album@users.noreply.github.com>
2023-09-02 19:37:11 +0530

    Add files via upload

--- src/Assets/data.js
+++ src/Assets/data.js
@@ -1,21 +1,22 @@
+import Img from '../Assets/images/hardisk.jpg'
 export const card_data= [
     {
         id: 1,
-        title: 'Fashion Brand',
-        description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-        imageUrl: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+        title: 'Hard Disk / Laptop',
+        description: "Hard drives lacks advanced recovery options, increasing the risk of permanent data loss. Data loss due to accidents or mishandling. Hard to access, needs laptop to access. ",
+        imageUrl: 'https://im.indiatimes.in/content/2023/May/vincent-botta-wYD_wfifJVs-unsplash_6454a63f5db27.jpg'
     },
     {
         id: 2,
-        title: 'Fashion Brand',
-        description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-        imageUrl: 'https://plus.unsplash.com/premium_photo-1668485968681-fa4a97d09331?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+        title: 'Pendrive / Flashdrive',
+        description: "Exposure to extreme heat, moisture, or magnetic fields can damage the pendrive and lead to data loss.  Lack of backup means there's no recovery option if data is lost from the pendrive.  Pendrives can get infected with viruses, leading to data loss or theft ",
+        imageUrl: 'https://www.usbmemorydirect.com/blog/wp-content/uploads/2022/01/flash_drives_and_compact_flash_cards.jpg'
     },
     {
         id: 3,
-        title: 'Fashion Brand',
-        description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
-        imageUrl: 'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80'
+        title: 'digiAlbum',
+        description: "Data is stored on cloud. Has backup on more that 6 servers, so dataloss is next to impossible. Its available for very cheap compared to other service providers. Unique and designed for Special events like Wedding, Engagement, birthdays, etc. ",
+        imageUrl: 'https://demo-tawny-two.vercel.app/static/media/logo.2b4059680e0bb35264d8.png'
     },
     
 ]
